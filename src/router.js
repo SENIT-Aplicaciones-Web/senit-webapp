4bcb2cd7a52954fdf4b83d90c3f3f9a5d87235e7
LeonardoGarcia-1 <leonardo_351461@hotmail.com>
2026-05-13 19:46:29 -0500

    fix: deploy

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import iamRoutes from './iam/presentation/iam-routes.js'
 import frontDeskRoutes from './front-desk/presentation/front-desk-routes.js'
 
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes
 })
 
